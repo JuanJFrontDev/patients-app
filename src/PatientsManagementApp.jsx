c9b231c09b0ba9308c3b0ba8590a44e8d490a0e1
unknown <jefri_23_93@hotmail.com>
2023-08-21 22:38:28 -0300

    My Web app

---
+++ src/PatientsManagementApp.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const PatientsManagementApp = () => {
+  return (
+    <>
+        <h1 className='title'>Patients Management App</h1>
+        <button className='btn-login'>LOGIN</button>
+    </>
+  )
+}
