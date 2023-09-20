e57d98f77226078e0b0c115b88356ff293bb705f
JuanJFrontDev <jefri_23_93@hotmail.com>
2023-09-19 21:11:56 -0300

    Frontend part of the index page almost finished

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './style.css'
 import { PatientsManagementApp } from './PatientsManagementApp.jsx'
+import { Main } from './pages/main'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <PatientsManagementApp />
+    {/* <PatientsManagementApp /> */}
+    <Main />
   </React.StrictMode>,
 )
